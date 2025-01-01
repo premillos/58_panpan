a933135c4b260cec2e4905aa0b631177c042e176
x.home <a411568765@gmail.com>
2025-01-01 22:10:13 +0800

    sp

--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-dialog v-model="dialogVisible" title="登录" width="500" :close-on-click-modal="false" :show-close="false">
-      <el-form :model="loginForm" ref="loginForm" :rules="rules" label-width="80px">
+      <el-form :model="loginForm" ref="loginFormRef" :rules="rules" label-width="80px">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="loginForm.username" autocomplete="off" placeholder="请输入用户名"></el-input>
         </el-form-item>
@@ -21,14 +21,15 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const emit = defineEmits(['loginSuccess'])
 const dialogVisible = ref(true) // 初始值应为 false
-const loginForm = ref({
+const loginFormRef = ref(null)
+const loginForm = reactive({
   username: '',
   password: ''
 })
-const rules = ref({
+const rules = reactive({
   username: [
     { required: true, message: '请输入用户名', trigger: 'blur' }
   ],
@@ -38,15 +39,14 @@ const rules = ref({
 })
 
 const handleLogin = () => {
-  loginForm.value.validate((valid) => {
+  loginFormRef.value.validate((valid) => {
     if (valid) {
-
-      if (loginForm.value.username === '58vip' && loginForm.value.password === '123456_58') {
+      if (loginForm.username === '58vip' && loginForm.password === '123456_58') {
         ElMessage('登录成功')
         dialogVisible.value = false
         emit('loginSuccess')
       } else {
-        alert('用户名或密码错误')
+        ElMessage.error('用户名或者密码错误')
       }
     } else {
       console.log('error submit!!')
